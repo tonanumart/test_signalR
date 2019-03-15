706f740cf0d51f96a9ef3b8259ca7d3b74745fcd
anumart.c <anumart.c@b-connex.net>
2019-03-15 10:00:47 +0700

    add example async in solution test

---
+++ TestNLog/TaskAsync/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace TaskAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Action";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Result :";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(52, 22);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(19, 13);
+            this.resultLabel.TabIndex = 2;
+            this.resultLabel.Text = "....";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Action2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ TestNLog/TaskAsync/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Full Asyn Recommemnded
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var task = DoSomethingAsync();
+            Console.WriteLine("Your time has cost cannot wait");
+            int result = await task;
+            Console.WriteLine("Your result DoSomethingAsync() = {0}", result);
+            resultLabel.Text = result.ToString();
+        }
+
+
+        public async Task<int> DoSomethingAsync()
+        {
+            Console.WriteLine("Start DoSomethingAsync()");
+            // In the real world, we would do something particular.. 
+            // For this example, we are only going to (asynchronously) wait 100ms
+            await Task.Delay(5000);
+            Console.WriteLine("DoSomethingAsync return result");
+            return 1;
+        }
+
+        #endregion
+
+
+        #region Synchonous Recommended
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HardWork();
+        }
+
+        private async void HardWork()
+        {
+            var resultTask = Task.Run(() => DoSomething());
+            Console.WriteLine("Your time has cost cannot wait");
+            int result = await resultTask;
+            resultLabel.Text = result.ToString();
+        }
+
+        private int DoSomething()
+        {
+            Console.WriteLine("Start DoSomething()");
+            for (int i = 0; i != Int32.MaxValue; ++i)
+                ;
+            Console.WriteLine("Finish DoSomething()");
+            return 42;
+        }
+
+        #endregion
+
+    }
+}
